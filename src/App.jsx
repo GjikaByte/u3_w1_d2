7369556f159d079a7e8617712e2a05c93c48371a
Andi Gjika <andi.gjika@outlook.com>
2025-09-30 17:36:13 +0200

    Reinstalle Bootastrap, cleaned directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-// import MyNav from './components/MyNav.jsx'
+import MyNav from './components/MyNav.jsx'
 import Welcome from './components/Welcome.jsx'
-// import AllTheBooks from './components/AllTheBooks.jsx'
+import AllTheBooks from './components/AllTheBooks.jsx'
 import MyFooter from './components/MyFooter.jsx'
 
 function App() {
 
   return (
     <>
-      {/* <MyNav /> */}
+      <MyNav />
       <main className="container">
         <Welcome />
-        {/* <AllTheBooks /> */}
+        <AllTheBooks />
       </main>
       <MyFooter />
     </>
